c8f1faf648974ef227d16b457f9976e7f11f4bfa
Miguel Romero <miky.rl@gmail.com>
2024-04-29 20:05:43 +0200

    Fix resume link

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -70,7 +70,7 @@ export const heroData: Hero = {
   ),
   actions: [
     {
-      href: 'assets/resume.pdf',
+      href: '/assets/resume.pdf',
       text: 'Resume',
       primary: true,
       Icon: ArrowDownTrayIcon,
