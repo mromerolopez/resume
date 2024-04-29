6ebc1fd1dcfb277eb05c8a27fd017d878044da37
Miguel Romero <miky.rl@gmail.com>
2024-04-29 19:59:27 +0200

    Fix header

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -1,13 +1,10 @@
 import {NextPage} from 'next';
 import Head from 'next/head';
-import {useRouter} from 'next/router';
 import {memo, PropsWithChildren} from 'react';
 
 import {HomepageMeta} from '../../data/dataDef';
 
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
-  const {} = useRouter();
-
   return (
     <>
       <Head>
@@ -16,10 +13,10 @@ const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, 
 
         {/* several domains list the same content, make sure google knows we mean this one. */}
 
-        <link href="/favicon.ico" rel="icon" sizes="any" />
-        <link href="/icon.svg" rel="icon" type="image/svg+xml" />
-        <link href="/apple-touch-icon.png" rel="apple-touch-icon" />
-        <link href="/site.webmanifest" rel="manifest" />
+        <link href="favicon.ico" rel="icon" sizes="any" />
+        <link href="icon.svg" rel="icon" type="image/svg+xml" />
+        <link href="apple-touch-icon.png" rel="apple-touch-icon" />
+        <link href="site.webmanifest" rel="manifest" />
 
         {/* Open Graph : https://ogp.me/ */}
         <meta content={title} property="og:title" />
