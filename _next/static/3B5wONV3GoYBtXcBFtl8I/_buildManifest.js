5ab8af8e94e9e8127734dfbdba8e29c053155b00
Miguel Romero <miky.rl@gmail.com>
2024-04-29 19:15:11 +0200

    initial commit

---
+++ _next/static/3B5wONV3GoYBtXcBFtl8I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/706-4c260c2d8e0d893c.js","static/chunks/pages/index-208b1fd368b37b2b.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
